7fa8046abf5aa9233946d1c61cab346d634e9fec
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-11-23 15:49:07 +0300

    theme-2

--- wp-content/themes/yuchikurovApp/index.php
+++ wp-content/themes/yuchikurovApp/index.php
@@ -16,6 +16,24 @@
 
 <body>
     <div class="main">There is page</div>
-    <div class="second"></div>
+    <div class="second">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2><?php the_title(); ?></h2>
+          </div>
+          <div class="col-md-12 seminar_content">
+            <?php
+            the_content(__('(more...)'));
+            edit_post_link(__('Edit This'));
+            ?>
+          </div>
+        </div>
+      </div>
+      <?php endwhile; else: ?>
+        <?php _e('Sorry, no posts matched your criteria.'); ?>
+      <?php endif; ?>
+    </div>
 </body>
 </html>
