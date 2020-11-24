51b3558063af395cba1bff59c416a8ac96744719
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-11-24 21:22:18 +0300

    theme-9

--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -1,11 +1,3 @@
 jQuery(document).ready(function () {
-  PullToRefresh.init({
-    mainElement: '#main', // above which element?
-    onRefresh: function (done) {
-      setTimeout(function () {
-        done(); // end pull to refresh
-        alert('refresh');
-      }, 1500);
-    }
-  });
+  
 });
