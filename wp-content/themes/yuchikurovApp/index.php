bdbbe92faf79753dbb7697c46c9656098da89205
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-11-24 15:27:14 +0300

    theme-3

--- wp-content/themes/yuchikurovApp/index.php
+++ wp-content/themes/yuchikurovApp/index.php
@@ -15,25 +15,28 @@
 </head>
 
 <body>
-    <div class="main">There is page</div>
-    <div class="second">
-      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <h2><?php the_title(); ?></h2>
-          </div>
-          <div class="col-md-12 seminar_content">
-            <?php
-            the_content(__('(more...)'));
-            edit_post_link(__('Edit This'));
-            ?>
-          </div>
+  <div class="main">There is page</div>
+  <div class="second">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2><?php the_title(); ?></h2>
+        </div>
+        <div class="col-md-12 seminar_content">
+          <?php
+          the_content(__('(more...)'));
+          edit_post_link(__('Edit This'));
+          ?>
         </div>
       </div>
-      <?php endwhile; else: ?>
-        <?php _e('Sorry, no posts matched your criteria.'); ?>
-      <?php endif; ?>
     </div>
+    <?php endwhile; else: ?>
+      <?php _e('Sorry, no posts matched your criteria.'); ?>
+    <?php endif; ?>
+  </div>
+  <?php wp_footer(); ?>
+  <script src="<?php bloginfo('template_url'); ?>/js/pull_ref.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
 </body>
 </html>
