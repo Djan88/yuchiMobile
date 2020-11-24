392e636d5f41d4b38102c3f6bb3bdded6032dfad
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-11-24 21:05:48 +0300

    theme-7

--- wp-content/themes/yuchikurovApp/index.php
+++ wp-content/themes/yuchikurovApp/index.php
@@ -38,7 +38,7 @@
     </div>
   </div>
   <?php wp_footer(); ?>
-  <script src="<?php bloginfo('template_url'); ?>/js/jquery.ui.touch-punch.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/hummer-touch.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/js/pull_ref.min.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
 </body>
