d3a91244e6f6cd27fe5b55dc56f9284ac2e7a92b
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-11-25 12:40:57 +0300

    theme-13

--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -1,8 +1,16 @@
 jQuery(document).ready(function () {
+  // Pull to refresh
   PullToRefresh.init({
     mainElement: '#all', // above which element?
     onRefresh: function (done) {
       window.location.reload();
     }
+  });
+  // Menu
+  var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
   });
 });
