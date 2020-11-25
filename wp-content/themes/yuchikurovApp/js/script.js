84f65713023fceea3aae49cd48fce6e0d8eee92b
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-11-25 15:54:53 +0300

    theme-51

--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -3,7 +3,7 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
     const platform = device.platform;
     screen.orientation.lock('portrait');
-    StatusBar.overlaysWebView(true);
+    StatusBar.overlaysWebView(false);
 }
 
 jQuery(document).ready(function () {
