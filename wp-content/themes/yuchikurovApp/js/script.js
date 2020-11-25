82a883a411e523804d7afa9636e57cc2c44a879c
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-11-25 13:46:56 +0300

    theme-27

--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -1,3 +1,13 @@
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+    alert('ready');
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+    window.addEventListener("orientationchange", function(){
+        alert(screen.orientation.type); // e.g. portrait
+    });
+}
+onDeviceReady();
 jQuery(document).ready(function () {
   // Pull to refresh
   PullToRefresh.init({
