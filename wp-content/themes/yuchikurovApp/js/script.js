dc781468cec38d20ba82b6ac24124dc9adf44a81
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-11-26 22:28:34 +0300

    theme-93

--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -3,22 +3,6 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
     // const platform = device.platform;
     screen.orientation.lock('portrait');
-
-    if(window.StatusBar) {
-        if($parameters.Style === 1) {
-            // Change Status Bar background bar to white
-            window.StatusBar.backgroundColorByName("white");
-            // Change Status Bar text color to black
-            window.StatusBar.styleDefault();
-        } else {
-            // Change Status Bar background bar to black
-            window.StatusBar.backgroundColorByName("black");
-            // Change Status Bar text color to white
-            window.StatusBar.styleLightContent();
-        }
-    } else {
-        alert("not present");
-    }
 }
 
 jQuery(document).ready(function () {
