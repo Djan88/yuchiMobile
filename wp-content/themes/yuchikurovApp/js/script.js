7c24aa1361d56720e7631fcaea17a59fb2affb14
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-11-26 13:10:07 +0300

    theme-71

--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -3,8 +3,9 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
     const platform = device.platform;
     screen.orientation.lock('portrait');
+    StatusBar.backgroundColorByHexString("#fff");
+    StatusBar.styleBlackTranslucent();
 }
-alert('true');
 
 jQuery(document).ready(function () {
   // Pull to refresh
