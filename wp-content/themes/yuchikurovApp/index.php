9d0151af9c33ef481fb7fb12987bc2f49c2e207d
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-11-28 12:13:57 +0300

    theme-154

--- wp-content/themes/yuchikurovApp/index.php
+++ wp-content/themes/yuchikurovApp/index.php
@@ -45,16 +45,16 @@
                 <!-- Swiper -->
                 <div class="swiper-container">
                   <div class="swiper-wrapper">
-                    <div class="swiper-slide">Slide 1</div>
-                    <div class="swiper-slide">Slide 2</div>
-                    <div class="swiper-slide">Slide 3</div>
-                    <div class="swiper-slide">Slide 4</div>
-                    <div class="swiper-slide">Slide 5</div>
-                    <div class="swiper-slide">Slide 6</div>
-                    <div class="swiper-slide">Slide 7</div>
-                    <div class="swiper-slide">Slide 8</div>
-                    <div class="swiper-slide">Slide 9</div>
-                    <div class="swiper-slide">Slide 10</div>
+                    <div class="swiper-slide">Отворение</div>
+                    <div class="swiper-slide">Травма</div>
+                    <div class="swiper-slide">Копье</div>
+                    <div class="swiper-slide">Фильтр</div>
+                    <div class="swiper-slide">Solis</div>
+                    <div class="swiper-slide">Живая Вода</div>
+                    <div class="swiper-slide">Мертвая Вода</div>
+                    <div class="swiper-slide">Ловушка</div>
+                    <div class="swiper-slide">Супер Дисфункция</div>
+                    <div class="swiper-slide">Супер Ловушка</div>
                   </div>
                   <!-- Add Pagination -->
                   <div class="swiper-pagination"></div>
