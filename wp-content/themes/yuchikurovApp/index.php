9f41ef679359671cc5b9f5e84076b38a2504c641
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-12-03 15:28:41 +0300

    footer-8

--- wp-content/themes/yuchikurovApp/index.php
+++ wp-content/themes/yuchikurovApp/index.php
@@ -7,7 +7,7 @@
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
   <meta name="viewport" content="initial-scale=1, width=device-width, height=device-height, viewport-fit=cover">
   <title><?php wp_title(''); ?></title>
-  <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css" rel="stylesheet">
+  <!-- <link href="<?php //bloginfo('template_url'); ?>/css/bootstrap.min.css" rel="stylesheet"> -->
   <link href="<?php bloginfo('template_url'); ?>/css/open-iconic-bootstrap.css" rel="stylesheet">
   <link href="<?php bloginfo('template_url'); ?>/css/swipe-bundle.min.css" rel="stylesheet">
   <link href="<?php bloginfo('template_url'); ?>/style.css" rel="stylesheet">
