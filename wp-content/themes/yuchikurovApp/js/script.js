f2d258bfd894447b62a9fca194a6a3be1c80d5fc
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-12-03 12:17:19 +0300

    theme-163

--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -29,6 +29,31 @@ jQuery(document).ready(function () {
       window.location.reload();
     }
   });
+
+  jQuery('.tabbar li a').on('click', function(e) {
+
+      e.preventDefault();
+
+      let that = $(this),
+          li = that.parent(),
+          ul = li.parent();
+
+      if(!ul.hasClass('move') && !li.hasClass('active')) {
+          ul.children('li').removeClass('active');
+
+          ul.css('--x-n', li.position().left + li.outerWidth() / 2 + 'px');
+          li.addClass('move');
+          ul.addClass('move');
+
+          setTimeout(() => {
+              ul.removeClass('move');
+              li.removeClass('move').addClass('active');
+              ul.css('--x', li.position().left + li.outerWidth() / 2 + 'px');
+          }, 1200);
+      }
+
+  });
+
   // Menu
   // var slideout = new Slideout({
   //   'panel': document.getElementById('panel'),
