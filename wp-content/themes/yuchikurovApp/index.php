cc4f2327397ffe689b36b1d94fb3f651ed538604
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-12-03 15:30:55 +0300

    footer-11

--- wp-content/themes/yuchikurovApp/index.php
+++ wp-content/themes/yuchikurovApp/index.php
@@ -62,7 +62,12 @@
       </symbol>
   </svg>
   <?php wp_footer(); ?>
-  
+  <script src="<?php bloginfo('template_url'); ?>/js/al.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/jquery.ui.touch-punch.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/pull_ref.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/mobile-nav.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/swiper-bundle.min.js"></script>
   <!-- <script src="<?php //bloginfo('template_url'); ?>/js/slideout.min.js"></script> -->
   <script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
 </body>
--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -1,10 +1,34 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+    // const platform = device.platform;
+    screen.orientation.lock('portrait');
     
+    var swiper = new Swiper('.swiper-container', {
+      direction: 'vertical',
+      slidesPerView: 1,
+      spaceBetween: 30,
+      mousewheel: true,
+      pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+      },
+    });
+
+    swiper.on('slideChange', function () {
+      navigator.vibrate(1000);
+    });
 }
 
 jQuery(document).ready(function () {
-  
+  // Pull to refresh
+  PullToRefresh.init({
+    mainElement: '#panel', // above which element?
+    triggerElement: '.header',
+    onRefresh: function (done) {
+      window.location.reload();
+    }
+  });
 
   jQuery('.tabbar li a').on('click', function(e) {
 
@@ -29,5 +53,6 @@ jQuery(document).ready(function () {
       }
 
   });
+
 
 });
