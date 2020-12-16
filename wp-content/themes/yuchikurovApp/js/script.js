ca984210cc17d263670c626515404d2ee9392ec8
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-12-16 14:46:59 +0300

    resources-59

--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -7,7 +7,6 @@ function onDeviceReady() {
     // swiper.on('slideChange', function () {
     //   navigator.vibrate(1000);
     // });
-    
     jQuery('.tabbar li a').on('click', function(event) {
       
     });
@@ -63,81 +62,10 @@ jQuery(document).ready(function () {
 
   });
 
-  jQuery('.box').on('click', function(event) {
-    jQuery('.app-main, .app-second').addClass('hidden').removeAttr('style');
-    jQuery('.app-slide').fadeIn(500).removeClass('hidden').css('display', 'flex');
-    jQuery('.header-text').text('ПОЛЕЗНЫЕ РЕСУРСЫ');
-    jQuery('.back-button').addClass('hidden');
-    jQuery('.slider-box').addClass('hidden').removeAttr('style');
-    jQuery('.app-slide-menu').fadeIn(500).removeClass('hidden').css('display', 'flex');
-    // screen.orientation.lock('portrait');
-    jQuery('.frame').removeClass('hidden');
-  });
-
-  jQuery('.home').on('click', function(event) {
-    jQuery('.app-slide, .app-second').addClass('hidden').removeAttr('style');
-    jQuery('.app-main').fadeIn(500).removeClass('hidden').css('display', 'flex');
-    jQuery('.header-text').text('ШКОЛА ДОКТОРА ЧИКУРОВА');
-    jQuery('.back-button').addClass('hidden');
-    // screen.orientation.lock('portrait');
-    jQuery('.frame').removeClass('hidden');
-  });
-
-  jQuery('.calendar').on('click', function(event) {
-    jQuery('.app-slide, .app-main').addClass('hidden').removeAttr('style');
-    jQuery('.app-second').fadeIn(500).removeClass('hidden').css('display', 'flex');
-    jQuery('.header-text').text('ПУСТОЙ РАЗДЕЛ');
-    jQuery('.back-button').addClass('hidden');
-    jQuery('.slider-box').addClass('hidden').removeAttr('style');
-    jQuery('.app-slide-menu').fadeIn(500).removeClass('hidden').css('display', 'flex');
-    // screen.orientation.lock('portrait');
-    jQuery('.frame').removeClass('hidden');
-  });
-
-  jQuery('.main-item-formuls').on('click', function(event) {
-    jQuery('.app-slide-menu').addClass('hidden').removeAttr('style');
-    jQuery('.slider-formuls').fadeIn(500).removeClass('hidden').css('display', 'flex');
-    jQuery('.header-text').text('ФОРМУЛЫ БЦ');
-    jQuery('.back-button').removeClass('hidden');
-  });
-  jQuery('.main-item-elems').on('click', function(event) {
-    jQuery('.app-slide-menu').addClass('hidden').removeAttr('style');
-    jQuery('.slider-elems').fadeIn(500).removeClass('hidden').css('display', 'flex');
-    jQuery('.header-text').text('ПЕРВОЭЛЕМЕНТЫ БЦ');
-    jQuery('.back-button').removeClass('hidden');
-  });
-  jQuery('.main-item-regress').on('click', function(event) {
-    jQuery('.app-slide-menu').addClass('hidden').removeAttr('style');
-    jQuery('.slider-regress').fadeIn(500).removeClass('hidden').css('display', 'flex');
-    jQuery('.header-text').text('РЕГРЕССИВНАЯ ШКАЛА');
-    jQuery('.back-button').removeClass('hidden');
-    // screen.orientation.lock('landscape');
-    jQuery('.frame').addClass('hidden');
-  });
-
-  jQuery('.back-button').on('click', function(event) {
-    jQuery('.slider-box').addClass('hidden').removeAttr('style');
-    jQuery('.app-slide-menu').fadeIn(500).removeClass('hidden').css('display', 'flex');
-    jQuery('.header-text').text('СПРАВОЧНИКИ');
-    jQuery('.back-button').addClass('hidden');
-    // screen.orientation.lock('portrait');
-    jQuery('.frame').removeClass('hidden');
-  });
-
-  // jQuery(".swiper-slide").swipe( {
-  //   swipeRight:function(event, direction, distance, duration, fingerCount) {
-  //     jQuery('.slider-box').addClass('hidden').removeAttr('style');
-  //     jQuery('.app-slide-menu').fadeIn(500).removeClass('hidden').css('display', 'flex');
-  //     jQuery('.header-text').text('СПРАВОЧНИКИ');
-  //     jQuery('.back-button').addClass('hidden');
-  //     screen.orientation.lock('portrait');
-  //     jQuery('.frame').removeClass('hidden');
-  //   },
-  //   threshold:0
-  // });
+  
 
   jQuery('.slider-box').addClass('hidden').removeClass('overscreen');
-  jQuery('.tabbar li a.home').click();
+  jQuery('.home').click();
 
 
 
