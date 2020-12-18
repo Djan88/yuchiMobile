9ee30af8aa12e1373641664f0e4c6515eae628ce
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-12-18 12:18:07 +0300

    resources-74

--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -82,7 +82,7 @@ jQuery(document).ready(function () {
     jQuery('.app-main, .app-second').addClass('hidden').removeAttr('style');
     jQuery('.app-slide').fadeIn(500).removeClass('hidden').css('display', 'flex');
     jQuery('.header-text').text('ПОЛЕЗНЫЕ РЕСУРСЫ');
-    jQuery('.back-button').addClass('hidden');
+    jQuery('.back-button, .reverce_clean_graph').addClass('hidden');
     jQuery('.slider-box').addClass('hidden').removeAttr('style');
     jQuery('.app-slide-menu').fadeIn(500).removeClass('hidden').css('display', 'flex');
     // screen.orientation.lock('portrait');
@@ -93,7 +93,7 @@ jQuery(document).ready(function () {
     jQuery('.app-slide, .app-second').addClass('hidden').removeAttr('style');
     jQuery('.app-main').fadeIn(500).removeClass('hidden').css('display', 'flex');
     jQuery('.header-text').text('ШКОЛА ДОКТОРА ЧИКУРОВА');
-    jQuery('.back-button').addClass('hidden');
+    jQuery('.back-button, .reverce_clean_graph').addClass('hidden');
     // screen.orientation.lock('portrait');
     // jQuery('.frame').removeClass('hidden');
   });
@@ -102,7 +102,7 @@ jQuery(document).ready(function () {
     jQuery('.app-slide, .app-main').addClass('hidden').removeAttr('style');
     jQuery('.app-second').fadeIn(500).removeClass('hidden').css('display', 'flex');
     jQuery('.header-text').text('ПУСТОЙ РАЗДЕЛ');
-    jQuery('.back-button').addClass('hidden');
+    jQuery('.back-button, .reverce_clean_graph').addClass('hidden');
     jQuery('.slider-box').addClass('hidden').removeAttr('style');
     jQuery('.app-slide-menu').fadeIn(500).removeClass('hidden').css('display', 'flex');
     // screen.orientation.lock('portrait');
@@ -113,19 +113,19 @@ jQuery(document).ready(function () {
     jQuery('.app-slide-menu').addClass('hidden').removeAttr('style');
     jQuery('.slider-formuls').fadeIn(500).removeClass('hidden').css('display', 'flex');
     jQuery('.header-text').text('ФОРМУЛЫ БЦ');
-    jQuery('.back-button').removeClass('hidden');
+    jQuery('.back-button, .reverce_clean_graph').removeClass('hidden');
   });
   jQuery('.main-item-elems').on('click', function(event) {
     jQuery('.app-slide-menu').addClass('hidden').removeAttr('style');
     jQuery('.slider-elems').fadeIn(500).removeClass('hidden').css('display', 'flex');
     jQuery('.header-text').text('ПЕРВОЭЛЕМЕНТЫ БЦ');
-    jQuery('.back-button').removeClass('hidden');
+    jQuery('.back-button, .reverce_clean_graph').removeClass('hidden');
   });
   jQuery('.main-item-regress').on('click', function(event) {
     jQuery('.app-slide-menu').addClass('hidden').removeAttr('style');
     jQuery('.slider-regress').fadeIn(500).removeClass('hidden').css('display', 'flex');
     jQuery('.header-text').text('РЕГРЕССИВНАЯ ШКАЛА');
-    jQuery('.back-button').removeClass('hidden');
+    jQuery('.back-button, .reverce_clean_graph').removeClass('hidden');
     // screen.orientation.lock('landscape');
     // jQuery('.frame').addClass('hidden');
   });
@@ -134,7 +134,7 @@ jQuery(document).ready(function () {
     jQuery('.slider-box').addClass('hidden').removeAttr('style');
     jQuery('.app-slide-menu').fadeIn(500).removeClass('hidden').css('display', 'flex');
     jQuery('.header-text').text('СПРАВОЧНИКИ');
-    jQuery('.back-button').addClass('hidden');
+    jQuery('.back-button, .reverce_clean_graph').addClass('hidden');
     // screen.orientation.lock('portrait');
     // jQuery('.frame').removeClass('hidden');
   });
@@ -144,7 +144,7 @@ jQuery(document).ready(function () {
       jQuery('.slider-box').addClass('hidden').removeAttr('style');
       jQuery('.app-slide-menu').fadeIn(500).removeClass('hidden').css('display', 'flex');
       jQuery('.header-text').text('СПРАВОЧНИКИ');
-      jQuery('.back-button').addClass('hidden');
+      jQuery('.back-button, .reverce_clean_graph').addClass('hidden');
       // screen.orientation.lock('portrait');
       // jQuery('.frame').removeClass('hidden');
     },
@@ -190,7 +190,7 @@ jQuery(document).ready(function () {
       stop: function( event, ui ) {
         console.log(handle_val);
         jQuery('.reverce_acept').removeAttr('style');
-        jQuery('.reverce_clean_graph').removeAttr('style')
+        jQuery('.reverce_clean_graph').removeClass('hidden')
         if (handle_val <= 60) {
           mode_speed = 2;
         } else if (handle_val > 60 && handle_val <= 111) {
@@ -202,6 +202,11 @@ jQuery(document).ready(function () {
         }
       }
     });
+  });
+  jQuery('.reverce-clean').on('click', function(event) {
+    jQuery( "#slider" ).slider( "value", 0 );
+    jQuery('.knife_rate').detach();
+    jQuery(this).addClass('hidden')
   });
 
 });
