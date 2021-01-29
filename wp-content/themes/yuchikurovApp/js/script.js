e3bf6749ce8cc93aec8cb08b2b82e64ce34ead2a
Ivan Avraamov <ivan.avraamov@gmail.com>
2021-01-29 13:34:04 +0300

    notification-14

--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -1,44 +1,93 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-    // const platform = device.platform;
-    screen.orientation.lock('portrait');
+  // Cordova is now initialized. Have fun!
+  // const platform = device.platform;
+  screen.orientation.lock('portrait');
 
-    // swiper.on('slideChange', function () {
-    //   navigator.vibrate(1000);
-    // });
-    jQuery('.tabbar li a').on('click', function(event) {
-      navigator.vibrate(25);
-    });
-    // Elements rotation
-    jQuery('.propeller_rotor').propeller({inertia: 0, speed: 0, onRotate: function(){ 
-      if (this.angle >= 15 && this.angle < 18) {
+  // swiper.on('slideChange', function () {
+  //   navigator.vibrate(1000);
+  // });
+  jQuery('.tabbar li a').on('click', function(event) {
+    navigator.vibrate(25);
+  });
+  // Elements rotation
+  jQuery('.propeller_rotor').propeller({inertia: 0, speed: 0, onRotate: function(){ 
+    if (this.angle >= 15 && this.angle < 18) {
+      navigator.vibrate(100);
+    } else if (this.angle >= 45 && this.angle < 48) {
+      navigator.vibrate(100);
+    } else if (this.angle >= 75 && this.angle < 78) {
+      navigator.vibrate(100);
+    } else if (this.angle >= 105 && this.angle < 108) {
+      navigator.vibrate(100);
+    } else if (this.angle >= 135 && this.angle < 138) {
+      navigator.vibrate(100);
+    } else if (this.angle >= 165 && this.angle < 168) {
+      navigator.vibrate(100);
+    } else if (this.angle >= 195 && this.angle < 198) {
+      navigator.vibrate(100);
+    } else if (this.angle >= 225 && this.angle < 228) {
+      navigator.vibrate(100);
+    } else if (this.angle >= 255 && this.angle < 258) {
+      navigator.vibrate(100);
+    } else if (this.angle >= 285 && this.angle < 288) {
+      navigator.vibrate(100);
+    } else if (this.angle >= 315 && this.angle < 318) {
+      navigator.vibrate(100);
+    } else if (this.angle >= 345 && this.angle < 348) {
+      navigator.vibrate(100);
+    }
+  }});
+  // Regressive centering
+
+  jQuery( function() {
+    var handle = jQuery("#custom-handle");
+    var handle_val;
+    jQuery("#slider").slider({
+      value: 7,
+      min: 0,
+      max: 200,
+      create: function() {
+        handle.text(jQuery(this).slider("value"));
+      },
+      slide: function( event, ui ) {
+        // handle.text( ui.value );
+        handle_val = ui.value;
+        knife = jQuery('body').find('#custom-handle').css('left');
+        knife = parseFloat(knife.substr(0, knife.length - 2)).toFixed();
+        knifeDate = new Date();
+        knifeDateDiff = knifeDate - knifeDateOld;
+        // console.log('test '+knifeDateDiff);
+        knife_rate_class = 'knife_rate-'+knife;
+        knife_rate_class_dotted = '.knife_rate-'+knife;
+        jQuery('.reverce_graph').append('<div class='+knife_rate_class+'></div>');
+        jQuery(knife_rate_class_dotted).addClass('knife_rate').css({
+          left: +knife+2+'px',
+          height: knifeDateDiff+'px'
+        });
+        knifeDateOld = knifeDate;
+      },
+      stop: function( event, ui ) {
+        console.log(handle_val);
         navigator.vibrate(100);
-      } else if (this.angle >= 45 && this.angle < 48) {
-        navigator.vibrate(100);
-      } else if (this.angle >= 75 && this.angle < 78) {
-        navigator.vibrate(100);
-      } else if (this.angle >= 105 && this.angle < 108) {
-        navigator.vibrate(100);
-      } else if (this.angle >= 135 && this.angle < 138) {
-        navigator.vibrate(100);
-      } else if (this.angle >= 165 && this.angle < 168) {
-        navigator.vibrate(100);
-      } else if (this.angle >= 195 && this.angle < 198) {
-        navigator.vibrate(100);
-      } else if (this.angle >= 225 && this.angle < 228) {
-        navigator.vibrate(100);
-      } else if (this.angle >= 255 && this.angle < 258) {
-        navigator.vibrate(100);
-      } else if (this.angle >= 285 && this.angle < 288) {
-        navigator.vibrate(100);
-      } else if (this.angle >= 315 && this.angle < 318) {
-        navigator.vibrate(100);
-      } else if (this.angle >= 345 && this.angle < 348) {
-        navigator.vibrate(100);
+        jQuery('.reverce-clean').removeClass('hidden')
+        if (handle_val <= 60) {
+          mode_speed = 2;
+        } else if (handle_val > 60 && handle_val <= 111) {
+          mode_speed = 4;
+        } else if (handle_val > 111 && handle_val <= 160) {
+          mode_speed = 6;
+        } else if (handle_val > 160) {
+          mode_speed = 8;
+        }
       }
-    }});
-
+    });
+  });
+  jQuery('.reverce-clean').on('click', function(event) {
+    jQuery( "#slider" ).slider( "value", 7 );
+    jQuery('.knife_rate').detach();
+    jQuery('.reverce-clean').addClass('hidden');
+  });
 }
 
 jQuery(document).ready(function () {
@@ -185,57 +234,5 @@ jQuery(document).ready(function () {
 
   jQuery('.slider-box, .formuls').addClass('hidden').removeClass('overscreen');
   jQuery('.tabbar li a.home').click();
-
-
-  // Regressive centering
-
-  jQuery( function() {
-    var handle = jQuery("#custom-handle");
-    var handle_val;
-    jQuery("#slider").slider({
-      value: 7,
-      min: 0,
-      max: 200,
-      create: function() {
-        handle.text(jQuery(this).slider("value"));
-      },
-      slide: function( event, ui ) {
-        // handle.text( ui.value );
-        handle_val = ui.value;
-        knife = jQuery('body').find('#custom-handle').css('left');
-        knife = parseFloat(knife.substr(0, knife.length - 2)).toFixed();
-        knifeDate = new Date();
-        knifeDateDiff = knifeDate - knifeDateOld;
-        // console.log('test '+knifeDateDiff);
-        knife_rate_class = 'knife_rate-'+knife;
-        knife_rate_class_dotted = '.knife_rate-'+knife;
-        jQuery('.reverce_graph').append('<div class='+knife_rate_class+'></div>');
-        jQuery(knife_rate_class_dotted).addClass('knife_rate').css({
-          left: +knife+2+'px',
-          height: knifeDateDiff+'px'
-        });
-        knifeDateOld = knifeDate;
-      },
-      stop: function( event, ui ) {
-        console.log(handle_val);
-        jQuery('.reverce_acept').removeAttr('style');
-        jQuery('.reverce-clean').removeClass('hidden')
-        if (handle_val <= 60) {
-          mode_speed = 2;
-        } else if (handle_val > 60 && handle_val <= 111) {
-          mode_speed = 4;
-        } else if (handle_val > 111 && handle_val <= 160) {
-          mode_speed = 6;
-        } else if (handle_val > 160) {
-          mode_speed = 8;
-        }
-      }
-    });
-  });
-  jQuery('.reverce-clean').on('click', function(event) {
-    jQuery( "#slider" ).slider( "value", 7 );
-    jQuery('.knife_rate').detach();
-    jQuery('.reverce-clean').addClass('hidden');
-  });
 
 });
