9a4ede855d593f7951cf2dc40663b32ef3a08834
Ivan Avraamov <ivan.avraamov@gmail.com>
2021-01-29 13:45:27 +0300

    notification-16

--- wp-content/themes/yuchikurovApp/index.php
+++ wp-content/themes/yuchikurovApp/index.php
@@ -239,7 +239,7 @@
             </div>
           </div>
           <div class="container app-second second hidden">
-            <div class="row" id="News">
+            <div class="row" id="news">
               <?php
               // The Query
               $query_news = new WP_Query( array( 'category_name' => 'news', 'posts_per_page' => '-1' ) );
