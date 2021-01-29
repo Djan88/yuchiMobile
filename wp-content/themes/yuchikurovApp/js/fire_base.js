3e11ca0be060bd5a29d53779236b2cdad4838d98
Ivan Avraamov <ivan.avraamov@gmail.com>
2021-01-29 10:24:59 +0300

    notification-12

--- wp-content/themes/yuchikurovApp/js/fire_base.js
+++ wp-content/themes/yuchikurovApp/js/fire_base.js
@@ -50,7 +50,6 @@ function onDevReady(){
     FirebasePlugin = window.FirebasePlugin;
     $output = $('#log-output');
     log("deviceready");
-    log(FirebasePlugin);
 
 
     // Set global error handler to catch uncaught JS exceptions
