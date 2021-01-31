c4d23dddeac2205ec47e5e85fef26f2f89a387cb
Ivan Avraamov <ivan.avraamov@gmail.com>
2021-01-31 14:06:49 +0300

    notification-77

--- wp-content/themes/yuchikurovApp/js/script.js
+++ wp-content/themes/yuchikurovApp/js/script.js
@@ -252,13 +252,13 @@ jQuery(document).ready(function () {
     count_animation = 1;
     vibroOne = setInterval(function(){
       if (count_animation <= count){
-      navigator.vibrate(50);
+      navigator.vibrate(100);
       count_animation += 1; 
       } else {
         clearInterval(vibroOne);
         count_animation = 1;
       }
-    }, 100);
+    }, 200);
   }
 
   jQuery( function() {
